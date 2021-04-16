d03bba528a13fc8a890a6a28d880f4324488602b
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-16 13:24:57 -0500

    commit tree 5a649a7797bad1e87a172a8167162ba3e690862c

---
+++ src/Messages.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default class Messages extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Messages</h2>
+        <ul>
+          <li>Message 1</li>
+          <li>Message 2</li>
+        </ul>
+      </div>
+    );
+  };
+}
+  
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home';
+import Login from './Login'
+import About from './About'
+import Navbar from './Navbar'
+import Signup from './Signup'
+import Messages from './Messages'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-const Home = () => {
-  return (
+ReactDOM.render((
+  <Router>
     <div>
-      <h1>Home!</h1>
+      <Navbar />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/signup' component={Signup} />
+      <Route exact path='/messages' component={Messages} />
     </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+  </Router>),
   document.getElementById('root')
 );
